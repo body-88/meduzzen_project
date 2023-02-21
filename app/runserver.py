a714f5317eba1b842bd523a898fd0882a5c899d9
Bogdan Pascari <b.pascari@gmail.com>
2023-02-21 08:57:54 +0200

    Fixing run app function

--- app/runserver.py
+++ app/runserver.py
@@ -1,12 +1,15 @@
 import uvicorn
 
-from app.core.config import settings
+from core.config import settings
 
 
 def run_uvicorn_server():
     uvicorn.run(
-        "app.main:app",
+        "main:app",
         host=settings.SERVER_HOST,
         port=settings.SERVER_PORT,
         reload=True,
     )
+    
+if __name__ == "__main__":
+    run_uvicorn_server()
