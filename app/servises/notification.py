a95334bbb00eb843d2a8aebb2a32100a7e7ff1eb
Bogdan Pascari <b.pascari@gmail.com>
2023-03-30 19:39:58 +0300

    fixes + refactoring

--- app/servises/notification.py
+++ app/servises/notification.py
@@ -3,7 +3,7 @@ from app.models.notifications import Notification
 from sqlalchemy import update, select
 from app.db.db_settings import get_db
 from fastapi import Depends, HTTPException
-
+from typing import List
 
 
 class NotificationService:
@@ -11,19 +11,16 @@ class NotificationService:
         self.db = db
 
 
-    async def get_all_notifications_unread(self, current_user_id: int):
-        query = select(Notification).where((Notification.user_id==current_user_id) & (Notification.status==False))
+    async def get_all_notifications(self, current_user_id: int, status: bool) -> List[Notification]:
+        if status == False:
+            query = select(Notification).where((Notification.user_id==current_user_id) & (Notification.status==status))
+            result = await self.db.fetch_all(query=query)
+        query = select(Notification).where((Notification.user_id==current_user_id) & (Notification.status==status))
         result = await self.db.fetch_all(query=query)
         return result
     
     
-    async def get_all_notifications_read(self, current_user_id: int):
-        query = select(Notification).where((Notification.user_id==current_user_id) & (Notification.status==True))
-        result = await self.db.fetch_all(query=query)
-        return result
-    
-    
-    async def get_my_notification(self, current_user_id: int, notification_id: int):
+    async def get_my_notification(self, current_user_id: int, notification_id: int) -> Notification:
         query = select(Notification).where(
             (Notification.user_id==current_user_id) & 
             (Notification.id==notification_id))
@@ -33,7 +30,7 @@ class NotificationService:
         return result
     
     
-    async def manage_notification(self, notification_id: int, current_user_id: int):
+    async def manage_notification(self, notification_id: int, current_user_id: int) -> Notification:
         db_notification = await self.get_my_notification(notification_id=notification_id, current_user_id=current_user_id)
         if db_notification.status == False:
             query = (
